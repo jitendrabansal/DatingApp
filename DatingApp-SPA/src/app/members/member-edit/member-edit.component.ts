3ae07a87503d71fc65b8730abeebe8516a1c1b87
jitbansa <jitbansa@WKWIN9753631>
2018-08-08 12:19:58 +0530

    End of Section 10

---
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { User } from '../../_models/user';
+import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from '../../_service/alertify.service';
+import { NgForm } from '@angular/forms';
+import { UserService } from '../../_service/user.service';
+import { AuthService } from '../../_service/auth.service';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+@ViewChild('editForm') editForm: NgForm;
+user: User;
+
+// For listening close event of browser and show confirm box
+@HostListener('window:beforeunload', ['$event'])
+unloadNotification($event: any) {
+  if (this.editForm.dirty) {
+    $event.returnValue = true;
+  }
+}
+
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService,
+        private userService: UserService, private authService: AuthService) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
+    });
+  }
+
+  updateUser() {
+    // console.log(this.user);
+    this.userService.updateUser(this.authService.decodedToken.nameid, this.user).subscribe(next => {
+      this.alertify.success('Profile updated successfully.');
+      this.editForm.reset(this.user);
+    }, error => {
+      this.alertify.error(error);
+    });
+
+  }
+
+}
